9b23a6678f49fd29374960990cdce866974fcf73
Marcelo Gobelli <marcelo@marcelo.gateway.sonic.net>
2019-12-30 15:14:07 -0800

    adding couple of python projects according to LPTHW

---
+++ tests/test_sllist_N.py
@@ -0,0 +1,18 @@
+from sllist_N import *
+from nose.tools import *
+
+
+def test_push():
+    colors = SingleLinkedList()
+    colors.push("Petalo blue")
+    assert colors.count() == 1
+
+
+
+
+
+
+
+    
+    
+
